726851dd10764ce446406a002d108d88ed6c6cdd
unknown <tu7d5w0m5@code.edu.az>
2022-04-05 23:43:12 -0800

    first commit

---
+++ 06042022task/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06042022task
+{
+    class Order
+    {
+        public Order()
+        {
+            _no++;
+            No = _no;
+        }
+        private static int _no;
+        public int No { get;}
+        public int ProductCount { get; set; }
+        public double TotalAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ 06042022task/Shop.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06042022task
+{
+    class Shop
+    {
+        List<Order> _orders;
+        public Shop()
+        {
+            _orders = new List<Order>();
+        }
+        public void AddOrder(Order order)
+        {
+            _orders.Add(order);
+        }
+        public double GetOrdersAvg()
+        {
+            if (_orders.Count!=0)
+            {
+                double AveragePrice = 0;
+                foreach (var order in _orders)
+                {
+                    AveragePrice = AveragePrice + order.TotalAmount;
+                }
+                return AveragePrice / _orders.Count;
+            }
+            return 0;
+        }
+        public double GetOrdersAvg(DateTime time)
+        {
+            var orders = _orders.FindAll(order=>order.CreatedAt > time);
+            double AveragePrice = 0;
+            foreach (var order in orders)
+            {
+                AveragePrice = AveragePrice + order.TotalAmount;
+            }
+            return AveragePrice / orders.Count;
+        }
+        public void RemoveOrderByno(int? no)
+        {
+            if (no==null)
+            
+                throw new Exception("Nulldir");
+            var order = _orders.Find(order => order.No == no);
+            if (order != null)
+                _orders.Remove(order);
+        }
+        public List<Order>FilterOrderByPrice(double MaxPrice,double MinPrice)
+        {
+            return _orders.FindAll(order => order.TotalAmount > MinPrice && order.TotalAmount < MaxPrice);
+        }
+    }
+}
